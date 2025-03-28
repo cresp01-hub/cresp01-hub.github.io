9ea0a9908ad7663fb18c9c235491718bef1cf96b
Mossab <mosaablachhab06@gmail.com>
2025-03-28 18:20:27 +0000

    testing with workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Pour la compatibilité avec GitHub Pages
+  trailingSlash: true,
+  // Si vous déployez sur un sous-domaine comme username.github.io/repo-name
+  basePath: '/cresp01-hub.github.io',
+};
 
 export default nextConfig;
